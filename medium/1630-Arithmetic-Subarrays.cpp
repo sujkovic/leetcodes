b548ce0158d3598e0d4d8ddd30f4b8599c5f3f27
sujkovic <adrian.sujkovic@gmail.com>
2023-11-23 17:33:37 -0500

    solve 1630-Arithmetic-Subarrays again by accident

--- medium/1630-Arithmetic-Subarrays.cpp
+++ medium/1630-Arithmetic-Subarrays.cpp
@@ -7,26 +7,84 @@
 //  return res vec
 
 //  let r = r.size(), n = nums.size()
-//  Time Complexity - O(r * nlogn) 
+//  Time Complexity - O(r * nlogn)
 //      outer loop r iterations (O(r))
 //          in it, sort vec of up to size n (O(nlogn))
 //          iterate through the vec of up to size n (O(n))
 //  Space Complexity - O(n) - curNums array can be up to size n
 //  Solved in 18 minutes
 
-class Solution {
+class Solution
+{
 public:
-    vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
+    vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l, vector<int> &r)
+    {
         vector<bool> res;
-        for (int i = 0; i < l.size(); i++) {
+        for (int i = 0; i < l.size(); i++)
+        {
             vector<int> curNums(nums.begin() + l[i], nums.begin() + r[i]);
             bool isArithmeticSubarray = true;
-            for (int j = 2; j < curNums.size(); j++) {
-                if ((curNums[j] - curNums[j - 1]) != (curNums[j - 1] - curNums[j - 2])) {
+            for (int j = 2; j < curNums.size(); j++)
+            {
+                if ((curNums[j] - curNums[j - 1]) != (curNums[j - 1] - curNums[j - 2]))
+                {
                     isArithmeticSubarray = false;
                 }
             }
             res.push_back(isArithmeticSubarray);
+        }
+        return res;
+    }
+};
+
+//  BRUH APPARANTLEY I DID THIS TWICE BY ACCIDENT WHOOPS
+//  heres my second run through i guess, surpsingly identical wow
+
+//  Make res vec, subArr vec
+//  Loop through l/r arrays
+//  For each subarray
+//      Copy the current subarray to a new vector and sort it
+//      If the abs value of the difference betweeen every consecutive element is ==, add true to res vec, else false
+//  Return res vec
+
+//  Solved in 14 min
+
+//  Time - O(n * nlogn)
+//      Outer loop worst case n iterations
+//      Within outer loop:
+//          sort cur sub array is nlogn
+//          loop through cur sub array is n at worst
+
+//  Space - O(n)
+//      Copy cur subArray to a new vec to sort it, worst case n (ex. l[i] = 0, r[i] = nums.size() - 1)
+
+class Solution
+{
+public:
+    vector<bool> checkArithmeticSubarrays(vector<int> &nums, vector<int> &l, vector<int> &r)
+    {
+        vector<bool> res;
+        vector<int> subArray;
+        int curSubArray = 0;
+        int numSubArrays = l.size();
+
+        while (curSubArray != numSubArrays)
+        {
+            subArray = vector<int>(nums.begin() + l[curSubArray], nums.begin() + r[curSubArray] + 1);
+            sort(subArray.begin(), subArray.end());
+            bool isArithmetic = true;
+
+            for (int i = 2; i < subArray.size(); i++)
+            {
+                if ((subArray[i] - subArray[i - 1]) != (subArray[i - 1] - subArray[i - 2]))
+                {
+                    isArithmetic = false;
+                    break;
+                }
+            }
+            res.push_back(isArithmetic);
+            subArray.clear();
+            curSubArray++;
         }
         return res;
     }
--- medium/1980-Find-Unique-Binary-String.cpp
+++ medium/1980-Find-Unique-Binary-String.cpp
@@ -1,7 +1,9 @@
 //  Brute force i think
-//  Make set, generate all possible strings of length n from 0 to n - 1 and add them to set 
+//  Make set, generate all possible strings of length n from 0 to n - 1 and add them to set
 //  Remove all strings from set that are in nums vec
 //  Return any string in set
+
+//  Solved in 30 min (zoned out for half the time i spent working on it tbh)
 
 //  Time complexity - O(n)
 //  Loop of size n, as well as n recursive calls
@@ -11,10 +13,13 @@
 //  Space complexity - O(n)
 //  Max depth of recursive call stack is n, max set size is also n
 
-class Solution {
+class Solution
+{
 public:
-    void generateStrings(unordered_set<string> &set, int &limit, int curIndex, string curString) {
-        if (curIndex == limit) {
+    void generateStrings(unordered_set<string> &set, int &limit, int curIndex, string curString)
+    {
+        if (curIndex == limit)
+        {
             return;
         }
         curString[curIndex] = '0';
@@ -24,26 +29,30 @@ public:
         curString[curIndex] = '1';
         set.insert(curString);
         generateStrings(set, limit, curIndex + 1, curString);
-
     }
-    string findDifferentBinaryString(vector<string>& nums) {
+    string findDifferentBinaryString(vector<string> &nums)
+    {
         unordered_set<string> set;
         int limit = ceil(log2(nums.size())) + 1;
         string originalString = "";
 
-        for (int i = 0; i < nums.size(); i++) {
+        for (int i = 0; i < nums.size(); i++)
+        {
             originalString += "0";
         }
 
         generateStrings(set, limit, 0, originalString);
 
-        for (int i = 0; i < nums.size(); i++) {
-            if (set.find(nums[i]) != set.end()) {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (set.find(nums[i]) != set.end())
+            {
                 set.erase(nums[i]);
             }
         }
 
-        for (auto &it: set) {
+        for (auto &it : set)
+        {
             return it;
         }
 
