5d05ad2e613ebeed5f34e2e4b1842d55efdd3226
sujkovic <adrian.sujkovic@gmail.com>
2024-11-15 16:10:54 -0500

    solve 167-Two-Sum-II-Input-Array-is-Sorted again

--- medium/167-Two-Sum-II-Input-Array-is-Sorted.cpp
+++ medium/167-Two-Sum-II-Input-Array-is-Sorted.cpp
@@ -1,35 +1,30 @@
-//  2 3 4      6
-//  l r
-//  l   r
+//  Can just two pointer it
+//  2, 7, 11, 15
+//  l         r     -> sum is 17, 17 > 9 so go smaller
+//  l     r            13 > 9
+//  l  r               9 = 9, return
 
-//  2 7 11 15
-//  l      r
-//  l   r
-//  l r
+//  Solved in 4 min
+//  Time - O(n)
+//  Space - O(1)
 
-class Solution
-{
+class Solution {
 public:
-    vector<int> twoSum(vector<int> &numbers, int target)
-    {
+    vector<int> twoSum(vector<int>& numbers, int target) {
         int left = 0;
         int right = numbers.size() - 1;
 
-        while (left < right)
-        {
+        while (left < right) {
             int sum = numbers[left] + numbers[right];
 
-            if (sum == target)
-            {
-                return {left + 1, right + 1};
+            if (sum < target) {
+                left++;
             }
-            if (sum > target)
-            {
+            else if (sum > target) {
                 right--;
             }
-            else
-            {
-                left++;
+            else {
+                return {left + 1, right + 1};
             }
         }
 
