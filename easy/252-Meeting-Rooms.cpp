4e6e6e1360ef48fb1ea5bc32f58975fe5d32f99e
sujkovic <adrian.sujkovic@gmail.com>
2024-11-19 23:37:39 -0500

    solve 253-Meeting-Rooms-II again

--- easy/252-Meeting-Rooms.cpp
+++ easy/252-Meeting-Rooms.cpp
@@ -1,3 +1,4 @@
+//  Solved October 2024
 //  SOLVED ON NEETCODE
 
 /**
@@ -30,6 +31,35 @@ public:
             if ((vec[i].first > vec[i - 1].first && vec[i].first < vec[i - 1].second) ||
                 (vec[i].first <= vec[i - 1].first && vec[i].second >= vec[i - 1].second))
             {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
+
+
+
+
+//  Solved November 2024
+
+//  basically asking are there any overlapping intervals
+//  aka given an interval a - b and c - d
+//      overlaps if a <= c < b
+//  ^ sort the input and just check for that condition every int[i], int[i - 1]
+
+//  Solved in 4 min, accepted on first compile
+//  Time - O(nlogn)
+//  Space - O(1)
+
+class Solution {
+public:
+    bool canAttendMeetings(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end());
+
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i - 1][0] <= intervals[i][0] && intervals[i][0] < intervals[i - 1][1]) {
                 return false;
             }
         }
